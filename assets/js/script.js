f190bbc70290fe6ed09a6766b02332b227939030
Amanda <amanda-heij@hotmail.com>
2018-05-24 10:06:57 +0200

    modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -65,6 +65,32 @@ function carousel() {
 
 /*-------- slider- slut --------*/
 
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
 /* Godt at vide start*/
 function showInfo1() {
   var x = document.getElementById("info1");

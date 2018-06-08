eaa18770acf8492da4c7c5bbaef499c4e5823749
Amanda <amanda-heij@hotmail.com>
2018-06-08 11:10:14 +0200

    mine rettelser

--- assets/js/script.js
+++ assets/js/script.js
@@ -65,23 +65,22 @@ function carousel() {
 
 /*-------- slider- slut --------*/
 
+/* ---  modal 1  --- */
+
 // Get the modal
 var modal = document.getElementById('myModal');
 
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
 // When the user clicks the button, open the modal
 btn.onclick = function() {
     modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
+// Close the Modal
+function closeModal() {
+  document.getElementById('myModal').style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
@@ -90,6 +89,61 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
+/* ---  modal 2  --- */
+
+// Get the modal
+var modal = document.getElementById('myModal2');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn2");
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// Close the Modal
+function closeModal() {
+  document.getElementById('myModal2').style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+/* ---  modal 3  --- */
+
+// Get the modal
+var modal = document.getElementById('myModal3');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn3");
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// Close the Modal
+function closeModal() {
+  document.getElementById('myModal3').style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+
+
+/* Modal - slut*/
 
 /* Godt at vide start*/
 function showInfo1() {
